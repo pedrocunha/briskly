c7df4b1e86e5660e65f5710f3cbd3f69de0fb61a
Pedro Cunha <pkunha@gmail.com>
2014-03-27 16:51:52 +0000

    Handle denormalized

--- lib/briskly/element.rb
+++ lib/briskly/element.rb
@@ -5,6 +5,7 @@ class Briskly::Element
   attr_reader :term
   attr_reader :data
 
+
   def initialize(term, data = nil)
     raise ArgumentError unless term
     @term = term
@@ -12,8 +13,54 @@ class Briskly::Element
   end
 
   # TODO: implement this method on C
+  # also maybe when storing, store already denormalized
   def matches?(keyword)
-    term.downcase.include? keyword.downcase
+    return false if keyword.length > term.length
+
+    keyword.length.times do |index|
+      compare = keyword[index]
+      target  = term[index]
+
+      next if self.class.isEqual(compare, target) || 
+              self.class.isEqualIgnoringCase(compare, target) ||
+              self.class.isEqualWithoutSpecialChars(compare, target) ||
+              self.class.isEqualWithoutLatinChars(compare, target)
+      
+      return false
+    end
+
+    true
   end
 
+  private
+  def self.special_chars
+    /-+/
+  end
+
+  def self.isEqual(compare, target)
+    compare == target
+  end
+
+  def self.isEqualIgnoringCase(compare, target)
+    compare.downcase == target.downcase
+  end
+
+  def self.isEqualWithoutSpecialChars(compare, target)
+    compare.gsub(special_chars, ' ') == target.gsub(special_chars, ' ')
+  end
+
+  def self.isEqualWithoutLatinChars(compare, target)
+    return false if !compare.match(/./) || !target.match(/./)
+
+    compare_index = ACCENTED_CHARS.index(compare) 
+    compare = compare_index ? NON_ACCENTED_CHARS[compare_index] : compare
+
+    target_index  = ACCENTED_CHARS.index(target)
+    target = target_index ? NON_ACCENTED_CHARS[target_index] : target
+
+    compare == target
+  end
+
+  ACCENTED_CHARS = "ÀÁÂÃÄÅàáâãäåĀāĂăĄąÇçĆćĈĉĊċČčÐðĎďĐđÈÉÊËèéêëĒēĔĕĖėĘęĚěĜĝĞğĠġĢģĤĥĦħÌÍÎÏìíîïĨĩĪīĬĭĮįİıĴĵĶķĸĹĺĻļĽľĿŀŁłÑñŃńŅņŇňŉŊŋÒÓÔÕÖØòóôõöøŌōŎŏŐőŔŕŖŗŘřŚśŜŝŞşŠšſŢţŤťŦŧÙÚÛÜùúûüŨũŪūŬŭŮůŰűŲųŴŵÝýÿŶŷŸŹźŻżŽž"
+  NON_ACCENTED_CHARS = "AAAAAAaaaaaaAaAaAaCcCcCcCcCcDdDdDdEEEEeeeeEeEeEeEeEeGgGgGgGgHhHhIIIIiiiiIiIiIiIiIiJjKkkLlLlLlLlLlNnNnNnNnnNnOOOOOOooooooOoOoOoRrRrRrSsSsSsSssTtTtTtUUUUuuuuUuUuUuUuUuUuWwYyyYyYZzZzZz"
 end
--- spec/briskly/element_spec.rb
+++ spec/briskly/element_spec.rb
@@ -3,10 +3,9 @@ require 'briskly/element'
 
 describe Briskly::Element do
 
+  subject { described_class.new('foo', { a: 2 }) }
+
   describe '#new' do
-
-    subject { described_class.new('foo', { a: 2 }) }
-
     it 'accepts a term and data' do
       expect(subject).to be_a described_class
     end
@@ -17,6 +16,39 @@ describe Briskly::Element do
 
     it 'allows access to its data' do
       expect(subject.data).to eql(a: 2)
+    end
+  end
+
+  describe '#matches?' do
+
+    subject { described_class.new('foo-bar-ão', { a: 2 }) }
+
+    it 'matches if beginning of word' do
+      expect(subject.matches?('fo')).to be_true
+    end
+
+    it 'does not match if not equal of beginning of word' do
+      expect(subject.matches?('oo')).to be_false
+    end
+
+    it 'does not match when keyword is bigger than the term' do
+      expect(subject.matches?('foo-bar-ãoa')).to be_false
+    end
+
+    it 'is case insensitive' do
+      expect(subject.matches?('FOO')).to be_true
+    end
+
+    it 'matches the space with the dash' do
+      expect(subject.matches?('foo bar')).to be_true
+    end
+
+    it 'ignores the accents' do
+      expect(subject.matches?('foo-bar-ao')).to be_true
+    end
+
+    it 'also ignores the accents on the keyword' do
+      expect(subject.matches?('foo-bar-aó')).to be_true
     end
   end
 
